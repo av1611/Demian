aec6cf6df06c38f3d5dbaca978273ef726369bfc
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-26 16:06:41 +0300

    Imported `DarknessVisible` and changed `MainWindow` UI.
    Closes #3.

--- src/Demian.Client/MainWindow.xaml.cs
+++ src/Demian.Client/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
 ﻿namespace Demian.Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
