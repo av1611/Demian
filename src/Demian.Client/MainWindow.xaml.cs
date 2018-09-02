aa26392b9eb0bc1e571a403cbcf86e01dd9fd590
JoshuaLight <ashkariel.eter@gmail.com>
2018-09-02 16:46:17 +0300

    Added `Save` and `Load` keyboard handlers.

--- src/Demian.Client/MainWindow.xaml.cs
+++ src/Demian.Client/MainWindow.xaml.cs
@@ -1,11 +1,21 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace Demian.Client
 {
     public partial class MainWindow
     {
+        public static readonly RoutedCommand SaveCommand = new RoutedCommand();
+        public static readonly RoutedCommand LoadCommand = new RoutedCommand();
+        
         private TextEditor _editor;
         private TextViewModel _viewModel;
+
+        static MainWindow()
+        {
+            SaveCommand.InputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            LoadCommand.InputGestures.Add(new KeyGesture(Key.O, ModifierKeys.Control));
+        }
         
         public MainWindow()
         {
@@ -18,6 +28,16 @@ namespace Demian.Client
             
             _viewModel = new TextViewModel(_editor.Document);
             _viewModel.Print();
+        }
+
+        private void OnSave(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Save");
+        }
+
+        private void OnLoad(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Load");
         }
     }
 }
