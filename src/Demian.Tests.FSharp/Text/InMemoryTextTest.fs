d1282738ae651df7dd3565712f5cd0a7afca1424
JoshuaLight <ashkariel.eter@gmail.com>
2018-09-17 23:51:48 +0300

    Refactored `InMemoryTextTest` F# version.

--- src/Demian.Tests.FSharp/Text/InMemoryTextTest.fs
+++ src/Demian.Tests.FSharp/Text/InMemoryTextTest.fs
@@ -7,15 +7,18 @@ open System;
 open Xunit;
 
 let at x = x
+let in' x = x
 
 let asText value = (value) |> InMemoryText
 let characters (text:InMemoryText) = text.Characters.ToArray()
 let asCharacters (value:string) = value.Select(fun x -> new Character(x)).ToArray()
 let writtenTo (value:string) (text:string) (at:int) = (text |> InMemoryText).Write(value, at)
 
+let write (value:string) (at:int) (text:string) = (text |> InMemoryText).Write(value, at)
+
 [<Fact>]
 let ``Text should fail to construct from null`` () =
-    (fun () -> asText null |> ignore) |> shouldFail
+    (fun () -> null |> asText |> ignore) |> shouldFail
     
 [<Theory>]
 [<InlineData("")>]
@@ -27,4 +30,4 @@ let ``Text constructed from string should contain same characters`` (value) =
     
 [<Fact>]
 let ``After writing "1" at 0 in "Hello" it should become "1Hello"``() =
-    "1" |> writtenTo "Hello" <| at 0 |> should equal "1Hello"
+    write "1" <| at 0 <| in' "Hello" |> should equal "1Hello"
