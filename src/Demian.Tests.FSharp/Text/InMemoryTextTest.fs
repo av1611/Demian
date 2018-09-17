442edfba2356f2a3354e8f9673c648d76029a6a7
JoshuaLight <ashkariel.eter@gmail.com>
2018-09-18 01:00:08 +0300

    Added some new tests for `Write` method in F# project.

--- src/Demian.Tests.FSharp/Text/InMemoryTextTest.fs
+++ src/Demian.Tests.FSharp/Text/InMemoryTextTest.fs
@@ -9,18 +9,21 @@ open Xunit;
 let at x = x
 let in' x = x
 
-let asText value = (value) |> InMemoryText
+let asText value = value |> InMemoryText
 let characters (text:InMemoryText) = text.Characters.ToArray()
 let asCharacters (value:string) = value.Select(fun x -> new Character(x)).ToArray()
-let writtenTo (value:string) (text:string) (at:int) = (text |> InMemoryText).Write(value, at)
 
-let write (part:string) (at:int) (value:string) =
+let write part at value =
     let text = value |> asText
-    text.Write(part, at) |> ignore;
+    text.Write(part, at) |> ignore
     text.AsString()
+    
+let successOfWrite part at value =
+    let text = value |> asText
+    text.Write(part, at).Success
 
 [<Fact>]
-let ``Text should fail to construct from null`` () =
+let ``Text should fail to construct from null``() =
     (fun () -> null |> asText |> ignore) |> shouldFail
     
 [<Theory>]
@@ -30,11 +33,39 @@ let ``Text should fail to construct from null`` () =
 [<InlineData("Ready to set the world on fire, hehehehehe.")>]
 let ``Text constructed from string should contain same characters`` (value) =
     value |> asText |> characters |> should equal (value |> asCharacters)
+
+[<Fact>]
+let ``When write "-" at 0 in "Hello" it should equal "-Hello"``() =
+    write "-" <| at 0 <| in' "Hello" |> should equal "-Hello"
+        
+[<Fact>]
+let ``When write "-" at 1 in "Hello" it should equal "H-ello"``() =
+    write "-" <| at 1 <| in' "Hello" |> should equal "H-ello"
     
 [<Fact>]
-let ``After writing "1" at 0 in "Hello" it should become "1Hello"``() =
-    write "1" <| at 0 <| in' "Hello" |> should equal "1Hello"
+let ``When write "-" at 5 in "Hello" it should equal "Hello-"``() =
+    write "-" <| at 5 <| in' "Hello" |> should equal "Hello-"
     
 [<Fact>]
-let ``After writing "1" at 1 in "Hello" it should become "H1ello"``() =
-    write "1" <| at 0 <| in' "Hello" |> should equal "1Hello"
+let ``When write "--" at 0 in "Hello" it should equal "--Hello"``() =
+    write "--" <| at 0 <| in' "Hello" |> should equal "--Hello"
+    
+[<Fact>]
+let ``When write "--" at 1 in "Hello" it should equal "H--ello"``() =
+    write "--" <| at 1 <| in' "Hello" |> should equal "H--ello"
+    
+[<Fact>]
+let ``When write "--" at 5 in "Hello" it should equal "Hello--"``() =
+    write "--" <| at 5 <| in' "Hello" |> should equal "Hello--"
+    
+[<Fact>]
+let ``Writing null should fail``() =
+    successOfWrite null <| at 0 <| in' "Hello" |> should equal false
+    
+[<Fact>]
+let ``Writing any string at -1 should fail``() =
+    successOfWrite "" <| at -1 <| in' "Hello" |> should equal false
+    
+[<Fact>]
+let ``Writing any string at 5 in "1234" should fail``() =
+    successOfWrite "" <| at 5 <| in' "1234" |> should equal false
